7cd17b68b47c894d42388c9c8e2078736ced47fd
Adewumi <owonikokoadewunmi@gmail.com>
2024-02-05 22:29:38 +0100

    card styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card (){
     return (
-        <div>
+        <div class="bg-white p-4 text-center rounded-md shadow-ms">
             <h2>Card Component</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam facere minima, totam dolorum autem qui quis eveniet doloribus consequatur temporibus,
                  pariatur iure doloremque alias dolores eum veniam harum molestias.</p>
