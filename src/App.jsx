14c0c2e81102b09ac1ae914ca6e5c5abd0e683d1
Adewumi <owonikokoadewunmi@gmail.com>
2024-02-08 22:41:01 +0100

    props.children

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,20 @@ function App() {
     <img class="rounded-md" src={banner} alt="" />
 
     <div  class="grid grid-cols-4 gap-10 my-4">
-    <Card rounded={true} flat={false}/>
-    <Card rounded={false} flat={true}/>
+    <Card rounded={true} flat={false}> 
+    <h2>Jejeet Tee,Black</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam facere minima, totam dolorum autem qui quis eveniet doloribus consequatur temporibus,
+                 pariatur iure doloremque alias dolores eum veniam harum molestias.</p>
+                 <button class="btn ">View</button>
+                 </Card>
+    <Card rounded={false} flat={true}>
+    <h2>Jejeet Tee,White</h2>
+    <button class="btn ">Click Me</button>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam facere minima, totam dolorum autem qui quis eveniet doloribus consequatur temporibus,
+                 pariatur iure doloremque alias dolores eum veniam harum molestias.</p>
+                 <p>Only $10</p>
+                 
+    </Card>
     
     </div>
     
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card (props){
     return (
         <div class="bg-white p-4 text-center" 
         classList={{"rounded-md":props.rounded,"shadow-md":!props.flat}}>
-            
+            {props.children}
         </div>
     )
 }
